d546c9887efea19a6b3f56f39eec4ed7a2051291
rokkay <rayaleonjosecarlos@gmail.com>
2025-02-11 23:01:05 +0100

    feat: implemented filament HasLabel on ThirdPartyStatus

--- app/Enums/ThirdPartyStatus.php
+++ app/Enums/ThirdPartyStatus.php
@@ -4,9 +4,30 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-enum ThirdPartyStatus: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum ThirdPartyStatus: string implements HasColor, HasLabel
 {
-    case Active = 'Active';
-    case Closed = 'Closed';
-    case Archived = 'Archived';
+    case Active = 'active';
+    case Closed = 'closed';
+    case Archived = 'archived';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Active => __('Active'),
+            self::Closed => __('Closed'),
+            self::Archived => __('Archived'),
+        };
+    }
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::Active => 'success',
+            self::Closed => 'danger',
+            self::Archived => 'gray',
+        };
+    }
 }
